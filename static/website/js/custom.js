131e53bf743f9e24d4a6bc68569f4f07becd39c6
Guilin Zhang <GuilinDev@gmail.com>
2024-01-10 23:49:07 -0500

    redesign the home page

--- static/website/js/custom.js
+++ static/website/js/custom.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
 
     // Event listener for date change
     $('#your-date').on('change', function() {
+        console.log('Date changed');
         const date = this.value;
         fetch(`/get-available-times/?date=${date}`)
             .then(response => response.json())
@@ -18,16 +19,6 @@ $(document).ready(function() {
                 });
             })
             .catch(error => console.error('Error:', error));
-    });
-
-    $("#your-date").datepicker({
-        beforeShowDay: function(date) {
-            var day = date.getDay();
-            // Enable only Tuesday, Thursday, and Saturday (1, 3, 5)
-            return [day == 1 || day == 3 || day == 5];
-        },
-        minDate: 0, // Disallow past dates
-        dateFormat: 'yy-mm-dd' // Format date as YYYY-MM-DD
     });
 
     // Function to populate time options
