09a42e1c0fdf1cb1be0194b13f674addad83a4df
Guilin Zhang <GuilinDev@gmail.com>
2024-01-12 21:02:47 -0500

    Refactor insurance and add ad page

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Appointment
+
 
 # Register your models here.
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'date', 'time', 'service')  # Adjust the fields as per your model
+    list_filter = ('date', 'service')  # Add filters if needed
+    search_fields = ('name', 'email')  # Add search functionality
+
+
+admin.site.register(Appointment, AppointmentAdmin)
